9821f3c9e51604363675eaa42e7b3c78b7491d6c
Trần Khang <67548714+whoamiiamme@users.noreply.github.com>
2020-08-06 01:57:42 +0700

    Update location.js
    tesd

--- tesd/src/services/location.js
+++ tesd/src/services/location.js
@@ -1,4 +1,4 @@
-const GEO_LOCATION_API_KEY = 'AIzaSyBs1lxFmjRDTQAJK2XYLG6frz1xqwRSH3M';
+const GEO_LOCATION_API_KEY = GEO_LOCATION_API_KEY;
 
 export function geoPosition() {
 	return new Promise((resolve) => {
